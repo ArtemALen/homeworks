31768fdbecec5b7c7058e940e19df0843276c71b
ArtemAlen <aalenkin92@gmail.com>
2022-02-07 13:24:57 +0300

    Homework1

--- app/src/main/java/com/example/homeworks/MainActivity.java
+++ app/src/main/java/com/example/homeworks/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.homeworks;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button button = findViewById(R.id.bt1);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.new_layout);
+
+            }
+        });
     }
 }
